d5d8a1eab4e71e12b67646134a37c3deabf5a069
Midhun Murali <midhunmurali776@gmail.com>
2025-01-20 14:59:28 +0530

    Otp Verification Completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MobileNoCheckScreen(),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MobileNoCheckScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
