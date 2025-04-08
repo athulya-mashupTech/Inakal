ca9e7eec8936b14a2424533cf98b4b8dd57acda2
Athulya_Ajay <karthaathulya@gmail.com>
2025-04-08 16:51:23 +0530

    edit profile updation

---
+++ lib/features/drawer/widgets/edit_profile_widgets/editable_number_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class EditableNumberWidget extends StatefulWidget {
+  final TextEditingController controller;
+  const EditableNumberWidget({super.key, required this.controller});
+
+  @override
+  State<EditableNumberWidget> createState() => _EditableNumberWidgetState();
+}
+
+class _EditableNumberWidgetState extends State<EditableNumberWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.6,
+      child: TextField(
+        keyboardType: TextInputType.number,
+        controller: widget.controller,
+        textAlign: TextAlign.end,
+        decoration: null,
+        onSubmitted: (value) {
+          setState(() {
+            widget.controller.text = value;
+          });
+        },
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+    );
+  }
+}
